2742b89c35b4b6cb5fadf604c455cf5e82c18ae4
TCOS-design <113992668+TCOS-design@users.noreply.github.com>
2024-08-18 03:36:51 +0200

    Weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,44 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
+
+console.log(response.data);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon />`;
+}
+
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+    let day = days[date.getDay()];
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
+    if (hours < 10){
+        hours = `0${hours}`;
+    }
+        return `${day} ${hours}:${minutes}`;
+    
 }
 
 function searchCity(city) {
