ac00017f119eb0e069ee21a675163496798b9e8c
TCOS-design <113992668+TCOS-design@users.noreply.github.com>
2024-08-20 09:36:54 +0200

    Added closing quote on my <img> class

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ console.log(response.data);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
